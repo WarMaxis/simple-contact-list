ad6a9cbb9098a34d53d87d78d4953c69ff6fe1fa
Michał Milanowski <mmilanow@intercars.eu>
2021-03-29 16:30:13 +0200

    feat: loading spinner added; load more button added

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,40 @@ import {PersonData} from "./types";
 function App() {
     const [data, setData] = React.useState<PersonData[]>([]);
     const [selected, setSelected] = React.useState([]);
+    const [isLoader, setLoader] = React.useState(false);
 
-    const fetchData = useCallback(async () => {
+    const fetchData = useCallback(async (mergeResults?: boolean) => {
+        setLoader(true);
+
         try {
             const fetchedData = await apiData();
 
-            setData(fetchedData)
-        } catch {
+            setLoader(false);
 
+            mergeResults ? setData(prevState => [...prevState, ...fetchedData]) : setData(fetchedData)
+        } catch {
+            setLoader(false);
         }
     }, []);
 
     useEffect(() => {
         fetchData();
-    });
-
-    //  TODO fetch contacts using apiData function, handle loading and error states
+    }, []);
 
     return (
         <div className="App">
+            {isLoader &&
+            (<div className="overlay">
+                <div className="loader"/>
+            </div>)
+            }
             <div className="selected">Selected contacts: {selected.length}</div>
             <div className="list">
                 {data.map((personInfo) => (
                     // @ts-ignore
                     <PersonInfo key={personInfo.id} data={personInfo}/>
                 ))}
+                {!!data.length && <button className="button" onClick={() => fetchData(true)}>Load more</button>}
             </div>
         </div>
     );
